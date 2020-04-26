309aba57ece05c5e18cde54a5bc7d099e94acd92
Samuel Loft <sam@devloft.co.uk>
2020-04-26 10:47:57 +0100

    Added TailwindUI with Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
