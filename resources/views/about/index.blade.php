8575335292f839df35d6fe6a2c8ac0465f8fc9d9
Samuel Loft <sam@devloft.co.uk>
2020-05-09 14:33:30 +0100

    Added github link to about

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="bg-light p-12 w-3/4 rounded shadow mx-auto text-white">
-        <div class="flex mb-10">
+    <div class="bg-light p-12 w-3/4 rounded shadow mx-auto text-white mb-10">
+        <div class="flex mb-5">
             <div class="w-3/4">
                 <h1 class="text-4xl font-bold tracking-tighter">About me</h1>
 
@@ -22,6 +22,21 @@
             <div class="w-1/4 flex justify-end">
                 <img class="h-24 rounded-full border border-white" src="{{ asset('images/me.png') }}" alt="It's me">
             </div>
+        </div>
+
+        <div class="mb-10">
+            <a href="https://github.com/samloft" target="_blank">
+                <button
+                    class="flex bg-dark py-2 px-4 rounded shadow-lg hover:opacity-75 border border-white font-semibold">
+                    Check me out on GitHub
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"
+                         class="ml-2">
+                        <path fill="currentColor"
+                              d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                    </svg>
+                </button>
+            </a>
         </div>
 
         <div>
