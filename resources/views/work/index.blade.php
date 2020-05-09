887750795111cdf089500fc8a441ba5e09b951a7
Samuel Loft <sam@devloft.co.uk>
2020-05-09 11:54:35 +0100

    Added hover effect to works page

--- resources/views/work/index.blade.php
+++ resources/views/work/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="bg-light p-12 w-3/4 rounded shadow mx-auto text-white">
+    <div class="bg-light p-12 w-3/4 rounded shadow mx-auto text-white mb-8">
         <h1 class="text-4xl font-bold tracking-tighter">My work</h1>
 
         <p class="text-gray-400 font-light leading-tight mb-3">
-            A selection of works I have completed.
+            A selection of works I have completed 👨‍💻.
         </p>
 
         <div class="mt-6 grid grid-cols-2 gap-4 lg:mt-8">
             <div class="col-span-1 flex justify-center browser">
-                <div class="window">
+                <div class="window relative">
                     <div class="titlebar">
                         <div class="buttons">
                             <div class="close"></div>
@@ -20,13 +20,22 @@
                         Title
                     </div>
                     <div class="content">
-                        <img class="object-fill" src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+                        <div class="overlay"></div>
+
+                        <img class="w-full h-auto"
+                             src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+
+                        <div class="text">
+                            <span class="block mb-5 text-lg">The project title</span>
+
+                            <button class="py-2 px-4 border rounded border-white hover:text-black hover:bg-white shadow">View 👨‍💻</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-span-1 flex justify-center browser">
-                <div class="window">
+                <div class="window relative">
                     <div class="titlebar">
                         <div class="buttons">
                             <div class="close"></div>
@@ -36,13 +45,22 @@
                         Title
                     </div>
                     <div class="content">
-                        <img class="object-fill" src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+                        <div class="overlay"></div>
+
+                        <img class="w-full h-auto"
+                             src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+
+                        <div class="text">
+                            <span class="block mb-5 text-lg">The project title</span>
+
+                            <button class="py-2 px-4 border rounded border-white hover:text-black hover:bg-white shadow">View 👨‍💻</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-span-1 flex justify-center browser">
-                <div class="window">
+                <div class="window relative">
                     <div class="titlebar">
                         <div class="buttons">
                             <div class="close"></div>
@@ -52,13 +70,22 @@
                         Title
                     </div>
                     <div class="content">
-                        <img class="object-fill" src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+                        <div class="overlay"></div>
+
+                        <img class="w-full h-auto"
+                             src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+
+                        <div class="text">
+                            <span class="block mb-5 text-lg">The project title</span>
+
+                            <button class="py-2 px-4 border rounded border-white hover:text-black hover:bg-white shadow">View 👨‍💻</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-span-1 flex justify-center browser">
-                <div class="window">
+                <div class="window relative">
                     <div class="titlebar">
                         <div class="buttons">
                             <div class="close"></div>
@@ -68,13 +95,22 @@
                         Title
                     </div>
                     <div class="content">
-                        <img class="object-fill" src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+                        <div class="overlay"></div>
+
+                        <img class="w-full h-auto"
+                             src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+
+                        <div class="text">
+                            <span class="block mb-5 text-lg">The project title</span>
+
+                            <button class="py-2 px-4 border rounded border-white hover:text-black hover:bg-white shadow">View 👨‍💻</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-span-1 flex justify-center browser">
-                <div class="window">
+                <div class="window relative">
                     <div class="titlebar">
                         <div class="buttons">
                             <div class="close"></div>
@@ -84,7 +120,16 @@
                         Title
                     </div>
                     <div class="content">
-                        <img class="object-fill" src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+                        <div class="overlay"></div>
+
+                        <img class="w-full h-auto"
+                             src="https://www.formget.com/wp-content/uploads/2019/12/LinkPeek-Website-Screenshot-API.png">
+
+                        <div class="text">
+                            <span class="block mb-5 text-lg">The project title</span>
+
+                            <button class="py-2 px-4 border rounded border-white hover:text-black hover:bg-white shadow">View 👨‍💻</button>
+                        </div>
                     </div>
                 </div>
             </div>
