a8474142c0f3dc4e196a5ed3a9e4c1505198d800
Samuel Loft <sam@devloft.co.uk>
2021-09-09 17:05:32 +0100

    Removed GA and added UseFathom

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,15 +11,8 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-72783833-3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-72783833-3');
-    </script>
+    
+    <script src="https://cdn.usefathom.com/script.js" data-site="NTSTFXWC" defer></script>
 
 </head>
 <body class="bg-dark antialiased">
